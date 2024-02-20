3320d25503130d091ed80050a10b790400f620b9
Murad Snouber <111646806+Mordon-YT@users.noreply.github.com>
2024-02-20 17:54:21 +0200

    Update isorunner.sh

--- isorunner.sh
+++ isorunner.sh
@@ -1,14 +1,22 @@
-if ! test -d "/media/iso"; then
+#!/bin/bash
+
+# Check if directory /media/iso doesn't exist, then create it
+if [ ! -d "/media/iso" ]; then
     mkdir /media/iso
-if
-download="wget -O /media/iso/templates/iso"
-if [[$x == y]]; then
-    read debianver
-    else;
-        debianver=12.5.0
 fi
+
+download="wget -O /media/iso/template/iso/"
+
+x="y"
+
+if [[ "$x" == "n" ]]; then
+    read -p "Type the version: " debianver
+else
+    debianver="12.5.0"
+fi
+
 archlink="https://geo.mirror.pkgbuild.com/iso/latest/archlinux-x86_64.iso"
-debnlink="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-${}-amd64-netinst.iso"
+debnlink="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-${debianver}-amd64-netinst.iso"
 
 ${download}arch.iso ${archlink}
 ${download}debian.iso ${debnlink}
